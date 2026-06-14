cb8db22fff3b8d3eb51254e0373886ff8dc6d2ca
mp1924 <mp1924@srmist.edu.in>
2026-06-14 17:44:59 +0530

    Update password.py

--- Smart-Passsword-Generator/password.py
+++ Smart-Passsword-Generator/password.py
@@ -1,19 +1,58 @@
 import random
 import string
 
-print("Password Generator")
+print("Smart Password Generator")
 
-length = int(input("Enter the length of the password: "))
+length = int(input("Enter password length: "))
 
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
+use_upper = input("Include uppercase? (y/n): ").lower() == "y"
+use_lower = input("Include lowercase? (y/n): ").lower() == "y"
+use_numbers = input("Include numbers? (y/n): ").lower() == "y"
+use_symbols = input("Include symbols? (y/n): ").lower() == "y"
 
-all_characters = letters + digits + symbols
+char_pool = ""
+
+if use_upper:
+    char_pool += string.ascii_uppercase
+
+if use_lower:
+    char_pool += string.ascii_lowercase
+
+if use_numbers:
+    char_pool += string.digits
+
+if use_symbols:
+    char_pool += string.punctuation
+
+
+if len(char_pool) == 0:
+    print(" Error: No character types selected!")
+    exit()
 
 password = ""
-
 for i in range(length):
-    password += random.choice(all_characters)
+    password += random.choice(char_pool)
+print("\nGenerated Password:", password)
 
-print("Generated Password:", password)
+def check_strength(password):
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+    if any(c.isupper() for c in password):
+        score += 1
+    if any(c.islower() for c in password):
+        score += 1
+    if any(c.isdigit() for c in password):
+        score += 1
+    if any(c in string.punctuation for c in password):
+        score += 1
+
+    if score <= 2:
+        return "Weak"
+    elif score in [3, 4]:
+        return "Medium"
+    else:
+        return "Strong"
+
+print("Password Strength:", check_strength(password))
