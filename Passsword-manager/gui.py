9edbc90dae5d50769ab9196d9897aac63c570081
mp1924 <mp1924@srmist.edu.in>
2026-06-16 14:20:42 +0530

    Create gui.py

---
+++ Passsword-manager/gui.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+
+app.title("Secure Password Manager")
+app.geometry("600x500")
+
+app.mainloop()
